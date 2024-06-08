2f3a8ed21937aa1b6868dcd2780709e99f309978
thawan-xyz <thawan.xyz@gmail.com>
2024-06-08 18:17:47 -0300

    initial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename E>
+class BSTNode {
+public:
+
+};
+
+template <typename T, typename E>
+class BinarySearchTree {
+public:
+
+private:
+
+};
+
+int main() {
+    return 0;
+}
